031053263620fd8c6eb031eb609d0a337a8149c0
moonir01 <mcmt@ymail.com>
2019-12-24 16:26:52 +0600

    24-12-2019_4:25PM_MVC_Code1st

---
+++ PrCodeFirst/PrCodeFirst/Models/Enrollment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrCodeFirst.Models
+{
+    public enum Grade
+    {
+    }
+}
---
+++ PrCodeFirst/PrCodeFirst/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrCodeFirst.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+     
+    }
+
+    public class Enrollment
+    {
+    }
+}
