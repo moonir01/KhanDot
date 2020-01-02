29072f8723bfb54a3f79a2f854609f77142dfe6b
Md. Monirozzaman Khan <monirkhan37@outlook.com>
2020-01-02 21:39:43 +0600

    02_01_2020_09:39PM

---
+++ Consol/Array_1/Array_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is for Array");
+            /*int[] number = new int[5];
+            number[0] = 4;
+            number[1] = 8;
+            number[2] = 15;
+            number[3] = 16;
+            number[4] = 23;
+            Console.WriteLine(number[1]);
+            Console.WriteLine(number.Length);
+            Console.ReadLine();
+            */
+         
+            String[] names = new String[]
+            {
+                "Monir","Abir","Rahim","Kalam","Baker","Rashedul","Shakail","Sabribna"
+            };
+
+            ////for লুপের মাধ্যামে প্রিন্ট করা.
+            /*
+           for (int i = 0; i < names.Length; i++)
+           {
+               Console.WriteLine(names[i]);
+           }
+           Console.ReadLine();
+           */
+            ////foreach লুপের মাধ্যামে প্রিন্ট করা
+            /*
+             foreach (string nam in names)
+             {
+                 Console.WriteLine(nam);
+
+             }
+             Console.ReadLine();
+             */
+            /// Array এর মাধ্যামে টেক্সট উল্টো করে লিখা
+            string khan = "Hello this is mr khan here what about your" +
+                " what you need for this how  many changes";
+            char[] charArray = khan.ToCharArray();
+            Array.Reverse(charArray);
+            foreach (char item in charArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Consol/forloop2019122/forloop2019122/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop2019122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello This is Mr. Khan");
+            Console.WriteLine((int)products.Lemon);
+            foreach (String item in Enum.GetNames(typeof(products)))
+                Console.WriteLine(item);
+            //নিচের লাইনটি ইফ এলস্ চেকের মাধ্যমে
+            Console.WriteLine(" Below the line for if else check ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                    Console.WriteLine("7 is Found");
+
+            }
+
+            Console.ReadLine();
+        }
+        enum products
+        {
+            Apple,
+            Orange,
+            Multa,
+            Onion,
+            Lemon,
+            Crusifurious,
+            Green_Chilly,
+            Qukamber,
+            Ginger,
+
+
+        }
+    }
+}
