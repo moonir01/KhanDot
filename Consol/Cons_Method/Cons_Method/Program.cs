41ee1ab07e03016bba83b1d48e9444d5dbcd605c
Md. Monirozzaman Khan <monirkhan37@outlook.com>
2020-01-04 08:39:43 +0600

    04_01_2020_08:39AM

--- Consol/Cons_Method/Cons_Method/Program.cs
+++ Consol/Cons_Method/Cons_Method/Program.cs
@@ -10,13 +10,62 @@ namespace Cons_Method
     {
         static void Main(string[] args)
         {
-            HelloWorld();
+            Console.WriteLine("Hi This is Reverse String Project");
+            Console.WriteLine("What is your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("In What city were your born?");
+            string city = Console.ReadLine();
+            // ReverseString1();
+            Console.Write("Results: ");
+
+            string reversedFirstName= ReverseString(firstName);
+            string reversLastName=ReverseString(lastName);
+            string reverseCity= ReverseString(city);
+            //////ReverseString মেথডের জন্য নিচের লাইনটি ব্যাবহার করা হয় ।
+            ////Console.Write(string.Format("{0} {1} {2}",
+            ////    reversedFirstName,
+            ////    reversLastName,
+            ////    reverseCity));
+            DisplayResult(reversedFirstName, reversLastName, reverseCity);
             Console.ReadLine();
         }
 
-        private static void HelloWorld()
+
+        //public static void ReverseString1()
+        //{
+        //    string message = "Hello World";
+        //    char[] messageArray = message.ToCharArray();
+        //    Array.Reverse(messageArray);
+        //    foreach (char item in messageArray)
+        //    {
+        //        Console.Write(item);////WriteLine লিখলে একটি ক্যারেক্টার এর পর এন্টার পরে নিচের লাইনে চলে যায় তাই Write ব্যাবহার করা হয়েছে,
+        //    }
+
+        //}
+
+
+/*
+ //// মেথডে Value পাস করা হয়েছে String Message এর মাধ্যামে
+ */
+
+        public static string ReverseString(string message)
         {
-            Console.WriteLine("Hello World");
+            
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return string.Concat(messageArray);
+        }
+        private static void DisplayResult(
+            string reversedFirstName,
+            string reversLastName,
+            string reverseCity)
+        {
+            Console.Write(string.Format("{0} {1} {2}",
+             reversedFirstName,
+             reversLastName,
+             reverseCity));
         }
     }
 }
